d195a5672c590d600cb083c3dce36ed117e9be03
Nicky <nicolasyanase18@gmail.com>
2025-05-07 18:38:26 -0300

    feat: script da Kyocera m3550 funcionando

--- KYOCERA-M3550.py
+++ KYOCERA-M3550.py
@@ -8,16 +8,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 ips = [
-    "172.16.9.199",
-    #"172.16.1.28", #desligada no PSI
-    "172.16.18.3",
-    "172.16.17.49", #FATURAMENTO 2 - vai sair por causa da 3055ridoh central de guias
-    "172.16.5.60", # UTI
+    "172.16.18.202",
     ]
 
 
 chrome_options = Options()
-chrome_options.add_argument("--headless=new")
+#chrome_options.add_argument("--headless=new")
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--window-size=1920,1080')
@@ -32,63 +28,48 @@ for ip in ips:
         # -------- 1. Obtendo o CONTADOR --------
         print(f"\nAcessando impressora no IP: {ip}")
         navegador.get(f"http://{ip}")
+        navegador.maximize_window()
         WebDriverWait(navegador, 20).until(
             EC.frame_to_be_available_and_switch_to_it((By.NAME, "wlmframe"))
         )
         menu_principal = WebDriverWait(navegador, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'P001_menu_main') and (contains(@class, 'closed') or contains(@class, 'opened'))][.//span[contains(text(), 'Dados do dispositivo')]]"))
+            EC.element_to_be_clickable((By.XPATH, "//div[font[contains(text(), 'Dados do dispositivo')]]"))
         )
-        print("Menu encontrado:", menu_principal.text)
+
         menu_principal.click()
 
-        submenu = WebDriverWait(navegador, 10).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, ".P001_menu_sub"))
-        )
         contador_item = WebDriverWait(navegador, 10).until(
-            EC.element_to_be_clickable((By.ID, "s81"))
+            EC.element_to_be_clickable((By.XPATH, "//span[font[contains(text(), 'Contador')]]"))
         )
         contador_item.click()
+        
         # Aguarda o iframe 'printingjobs' aparecer e troca para ele
         WebDriverWait(navegador, 10).until(
-        EC.frame_to_be_available_and_switch_to_it((By.ID, "deviceconfig"))
+            EC.frame_to_be_available_and_switch_to_it((By.ID, "deviceconfig"))
         )
         # tr que possui um td com o texto total
         tr_contador = navegador.find_element(By.XPATH, "//tr[td[contains(text(), 'Total')]]")
+
         contador = tr_contador.find_element(By.XPATH, "./td[3]").text
 
 
         # -------- 2. Obtendo o ID da Impressora 
         navegador.get(f"http://{ip}")
-        # Depuração: liste todos os frames encontrados
-        frames = navegador.find_elements(By.TAG_NAME, "frame")
-        #print(f"Frames encontrados: {[frame.get_attribute('name') for frame in frames]}")
-
-        # Troca para o frame onde está o menu (nome correto: 'wlmframe')
+        navegador.maximize_window()
         WebDriverWait(navegador, 20).until(
             EC.frame_to_be_available_and_switch_to_it((By.NAME, "wlmframe"))
         )
-        # Depuração: imprime o HTML da página dentro do frame
-        #print(navegador.page_source)
-
-        # Espera até que o menu principal 'Dados do dispositivo' esteja clicável (aberto ou fechado)
         menu_principal = WebDriverWait(navegador, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'P001_menu_main') and (contains(@class, 'closed') or contains(@class, 'opened'))][.//span[contains(text(), 'Dados do dispositivo')]]"))
+            EC.element_to_be_clickable((By.XPATH, "//div[font[contains(text(), 'Dados do dispositivo')]]"))
         )
-        #print("Menu encontrado:", menu_principal.text)
+
         menu_principal.click()
 
-        # Aguarda o submenu aparecer, senão aparecer lança um exception
-        submenu = WebDriverWait(navegador, 10).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, ".P001_menu_sub"))
+        contador_item = WebDriverWait(navegador, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[font[contains(text(), 'Configuração')]]"))
         )
-
-        # Aguarda o item "Configuração" ficar clicável pelo ID
-        configuracao_item = WebDriverWait(navegador, 10).until(
-            EC.element_to_be_clickable((By.ID, "s80"))
-        )
-        # Clica no item "Configuração"
-        configuracao_item.click()
-        # Aguarda o iframe 'printingjobs' aparecer e troca para ele
+        contador_item.click()
+        
         WebDriverWait(navegador, 10).until(
             EC.frame_to_be_available_and_switch_to_it((By.ID, "printingjobs"))
         )
@@ -97,7 +78,7 @@ for ip in ips:
         # Obtém o texto do serial number
         id_printer = serial_span[3].text
 
-        # -------- Resultado final --------
+        # # -------- Resultado final --------
         print(f"Contador: {contador}")
         print(f"ID Impressora: {id_printer}")
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
- WebDriverWait(navegador, 10).until(
-        EC.frame_to_be_available_and_switch_to_it((By.ID, "deviceconfig"))
-        )
+time.sleep(2)
